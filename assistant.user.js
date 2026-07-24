74f9c54b8630a794b63271e086ee4d0055b4d56f
3DRaph <raphael.cherfils76@gmail.com>
2026-07-24 12:56:21 +0200

    Update assistant.user.js

--- assistant.user.js
+++ assistant.user.js
@@ -1,1 +1,33 @@
+// ==UserScript==
+// @name         3D Printing Assistant
+// @namespace    https://github.com/Raph24/3D-Printing-Assistant
+// @version      0.1.0
+// @description  Enhance Printables, MakerWorld, Cults3D and Thingiverse with productivity tools, print cost calculator and more.
+// @author       Raph24
+// @license      MIT
 
+// Sites pris en charge
+// @match        https://www.printables.com/*
+// @match        https://makerworld.com/*
+// @match        https://cults3d.com/*
+// @match        https://www.thingiverse.com/*
+
+// Exécution
+// @run-at       document-idle
+
+// Permissions
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+
+// Mise à jour (à compléter plus tard)
+// @downloadURL  https://raw.githubusercontent.com/Raph24/3D-Printing-Assistant/main/assistant.user.js
+// @updateURL    https://raw.githubusercontent.com/Raph24/3D-Printing-Assistant/main/assistant.user.js
+
+// ==/UserScript==
+
+(function () {
+    "use strict";
+
+    console.log("🖨 3D Printing Assistant v0.1.0 chargé !");
+})();
